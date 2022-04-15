08b1117ae4800cf6885dfd13dd1a938eafd54d50
Margarita Lazareva <mrgrtlzrv@gmail.com>
2022-04-15 13:53:32 +0700

    clonescript

---
+++ Assets/LearnMaterials 2/Scripts/Part2/CloneScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneScript : BaseScript
+{
+    private Transform myTransform;
+
+    [SerializeField]
+    [Min(1)]
+    private int count;
+
+    [SerializeField]
+    private float interval;
+
+    [SerializeField]
+    GameObject prefab;
+
+    void Start()
+    {
+        myTransform = transform;
+    }
+
+    [ContextMenu("Start")]
+    public override void Use()
+    {
+        Vector3 vector = new Vector3(myTransform.position.x, myTransform.position.y, myTransform.position.z);
+
+        for (int i = 0; i < count; i++)
+        {
+            vector = new Vector3(vector.x, vector.y, vector.z + interval);
+            Instantiate(prefab, vector, Quaternion.identity);
+        }
+    }
+}
+
